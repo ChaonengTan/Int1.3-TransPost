65b2a1d452851bbeb2d0b0892259a419cfe09cb5
ChaonengTan <chaonengtan@gmail.com>
2021-03-16 16:13:34 -0700

    +mongoDB, api, templates

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 import os
 import requests
+import json
 
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, redirect, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 app = Flask(__name__)
 
 load_dotenv()
 
+app.config["MONGO_URI"] = os.getenv('mongoURI')
+mongo = PyMongo(app)
+
 API_KEY = os.getenv('API_KEY')
-API_URL = ''
+API_URL = 'https://google-translate1.p.rapidapi.com/language/translate/v2/languages'
 
 # routes
 
