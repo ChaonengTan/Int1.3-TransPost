da6fbebc8a7c2d5683c4ca0cfe06ac858bfaeb60
ChaonengTan <chaonengtan@gmail.com>
2021-03-16 17:00:11 -0700

    +connected to mongoDB, postable to mongo

--- app.py
+++ app.py
@@ -8,12 +8,9 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
-
 load_dotenv()
-
 app.config["MONGO_URI"] = os.getenv('mongoURI')
 mongo = PyMongo(app)
-
 API_KEY = os.getenv('API_KEY')
 API_URL = 'https://google-translate1.p.rapidapi.com/language/translate/v2/languages'
 
@@ -22,7 +19,23 @@ API_URL = 'https://google-translate1.p.rapidapi.com/language/translate/v2/langua
 @app.route('/')
 def home():
     """Disc"""
+    mongoPosts = mongo.db.posts.find()
     context = {
+        'mongoPosts' : mongoPosts
+    }
+    return render_template('home.html', **context)
+@app.route('/newPost', methods=['GET', 'POST'])
+def newPost():
+    """Creates a new post"""
+    post = {
+        'title': request.form.get('newPostTitle'),
+        'image': request.form.get('newPostURL'),
+        'disc': request.form.get('newPostDisc')
+    }
+    mongo.db.posts.insert_one(post)
+    mongoPosts = mongo.db.posts.find()
+    context = {
+        'mongoPosts' : mongoPosts
     }
     return render_template('home.html', **context)
 
