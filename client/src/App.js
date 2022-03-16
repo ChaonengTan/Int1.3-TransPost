04c3f1403319fc28042b59533c6456d5b2660e92
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-03-15 23:18:17 -0700

    postRoute

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,15 @@ import { HashRouter as Router, Route } from 'react-router-dom'
 
 import Navbar from './components/navbar'
 import Home from './components/home'
+import Post from './components/post';
+
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar/>
         <Route exact path="/" component={Home}/>
+        <Route path="post/:id" component={Post}/>
         <div>footer goes here</div>
       </div>
     </Router>
