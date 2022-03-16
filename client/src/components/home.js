25692d5fddbfcda794fa9a8e6e83b4f8e8d1c03a
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-03-15 23:18:03 -0700

    home

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,10 +1,23 @@
-export default function Home() {
+import { Link } from 'react-router-dom'
+
+export default function Home(props) {
+    const { data } = props
+    const posts = data.map(post => {
+        return(
+            <div>
+                this is a post
+                <Link to={`/post/${post._id}`}>
+                    {post.title}
+                </Link>
+                <p>{post.message}</p>
+                <p>{post.author}</p>
+            </div>
+        )
+    })
     return(
         <div>
             this is the home page
-            <div>
-                this is a post
-            </div>
+            {posts}
         </div>
     )
 }
