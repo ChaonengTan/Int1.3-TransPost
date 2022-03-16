4e02fe8d98e32f4ad513c84efd924aaa8bf7858c
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-03-15 17:21:33 -0700

    +API_KEY, +mongoURI, +translatePost, +C, R operations(CRUD)
    added apikey for google translate API
    
    added URI for mongoDB
    
    added single post creation for crud operations into the DB
    
    added findOne for crud operations for DB
    
    added findAll for crud operations for DB

--- server/server.js
+++ server/server.js
@@ -2,41 +2,83 @@
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 const { buildSchema } = require('graphql')
-const fetch = require('node-fetch')
-const { GraphQLJSON, GraphQLJSONObject } = require('graphql-type-json');
 const cors = require('cors')
+const request = require('request')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+import { API_KEY, URI } from './env'
+
+// init
+const client = new MongoClient(URI, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 // schema
 const schema = buildSchema(`
     type Post {
         id: String!
+        title: String!
         message: String!
         author: String!
     }
     type Query {
-        getAllPosts(): Data!
+        getAllPosts(): [Post!]!
+        translatePost(title: String!, message: String!): Post!
     }
 `)
 
 // resolvers
 const root = {
-    getWorldstate: async (location) => {
-        // fetch
-        const url = (location) => `https://api.warframestat.us/pc/${location.location}`
-        const res = await fetch(url(location))
-        const json = await res.json()
-
-        // standard returns
-        const activation = json.activation
-        const expiry = json.expiry
-        const id = json.id
-        const state = json.state ? json.state : json.active
-
-        // timeLeft
-        const currentTime = new Date()
-        const expTime = new Date(expiry)
-        const timeLeft = (expTime-currentTime)/1000
-        return { activation, expiry, id, state, timeLeft }
+    translatePost: async (title, message) => {
+        // perameters
+        const options = {
+            method: 'POST',
+            url: 'https://google-translate1.p.rapidapi.com/language/translate/v2/detect',
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded',
+                'accept-encoding': 'application/gzip',
+                'x-rapidapi-key': API_KEY,
+                'x-rapidapi-host': 'google-translate1.p.rapidapi.com',
+                useQueryString: true
+            },
+            form: {q: title, q: message}
+        }
+        // execute request
+        request(options, (error, response, body) => {
+            if (error) throw new Error(error);
+            console.log(body)
+            return body
+        })
+    },
+    createPost: async (post) => {
+        // connect to the db
+        client.connect(err => {
+            const collection = client.db("transpost").collection("post");
+            // inserts a post to collection
+            collection.insertOne(post)
+        })
+        client.close();
+    },
+    findOne: async (id) => {
+        let result
+        // connect to the db
+        client.connect(err => {
+            const collection = client.db("transpost").collection("post");
+            // finds one post by id
+            result = collection.findOne({_id: id})
+        })
+        client.close();
+        return result ? result : new Error('db findOne error')
+    },
+    findAll: async () => {
+        let result
+        // connect to the db
+        client.connect(err => {
+            const collection = client.db("transpost").collection("post");
+            // gets all results
+            result = collection.find()
+            
+        })
+        client.close();
+        return result ? result : new Error('db findAll error')
     }
 }
 
