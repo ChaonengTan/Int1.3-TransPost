150e46e951e14d4f5ed0b3f31793860b2af78b65
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-03-16 20:33:05 -0700

    translatePost (WIP)
    single query limited working
    
    working on getting two queries on a single call.

--- server/server.js
+++ server/server.js
@@ -20,13 +20,13 @@ type User {
     password: String
 }
 type Post {
-    _id: String!
+    _id: String
     title: String
     message: String
     author: String
 }
 type Query {
-    translatePost(title: String!, message: String!): Post!
+    translatePost(title: String!, message: String!, target: String!): JSON
     findOne(id: String!): Post!
     findAll: [Post!]
     verifyUser(username: String!, password: String!): User
@@ -39,23 +39,24 @@ type Mutation {
 
 // resolvers
 const root = {
-    translatePost: async (title, message) => {
+    translatePost: async ({title, message, target}) => {
         // perameters
         const options = {
             method: 'POST',
-            url: 'https://google-translate1.p.rapidapi.com/language/translate/v2/detect',
+            url: 'https://google-translate1.p.rapidapi.com/language/translate/v2',
             headers: {
-                'content-type': 'application/x-www-form-urlencoded',
-                'accept-encoding': 'application/gzip',
-                'x-rapidapi-key': process.env.API_KEY,
-                'x-rapidapi-host': 'google-translate1.p.rapidapi.com',
-                useQueryString: true
+              'content-type': 'application/x-www-form-urlencoded',
+              'accept-encoding': 'application/gzip',
+              'x-rapidapi-key': process.env.API_KEY,
+              'x-rapidapi-host': 'google-translate1.p.rapidapi.com',
+              useQueryString: true
             },
-            form: {q: title}
-        }
+            form: {q: title, target}
+          }
         // execute request
-        request(options, (error, response, body) => {
+        return request(options, (error, response, body) => {
             if (error) throw new Error(error)
+            console.log(body)
             return body
         })
     },
