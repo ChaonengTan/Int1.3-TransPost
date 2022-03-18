76691ef56be940bba58e385a6125158a6f9d63a3
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-03-17 21:06:14 -0700

    logout

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -2,13 +2,24 @@ import logo from '../static/images/logo.png'
 import './styles/navbar.css'
 export default function Navbar() {
     const username = sessionStorage.getItem('user') ? JSON.parse(sessionStorage.getItem('user')).username : false
+    const loggedIn = () => {
+        return(
+            <div>
+                <p>{username}</p>
+                <button onClick={() => {
+                    sessionStorage.removeItem('user')
+                    window.location.reload(false)
+                }}>Logout</button>
+            </div>
+        )
+    }
     return(
         <div className='navbar'>
             <div className='navLeft'>
                 <a href="/"><img src={logo} alt=''/>TransPost</a>
             </div>
             <div className='navRight'>
-                {username && username}
+                {username && loggedIn()}
             </div>
         </div>
     )
