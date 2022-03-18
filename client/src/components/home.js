d25e9e4140ce27dbbe2760d303dbcf4eaee9031a
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-03-17 20:43:23 -0700

    +register

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -4,14 +4,23 @@ import CreatePost from './createPost'
 import PostList from './postList'
 import findAll from '../queries/findAll'
 import Login from './login'
+import Register from './register'
 export default function Home() {
     const user = sessionStorage.getItem('user') ? JSON.parse(sessionStorage.getItem('user'))._id : false
     const { data } = useQuery(findAll, {
         pollInterval: 3000
     })
+    const loginRegister = () => {
+        return(
+            <div>
+                <Login/>
+                <Register/>
+            </div>
+        )
+    }
     return(
         <div>
-            { user ? <CreatePost/> : <Login/> }
+            { user ? <CreatePost/> : loginRegister() }
             { data ? <PostList data={data}/> : "Loading" }
         </div>
     )
---
+++ client/src/components/register.js
@@ -0,0 +1,43 @@
+import { useState, useEffect, useRef } from "react"
+import { useMutation } from "@apollo/client"
+import createUser from '../queries/createUser'
+
+export default function Register() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [addUser, { data }] = useMutation(createUser, {
+        variables: { username, password }
+    })
+    const dataRef = useRef(data)
+    useEffect(() => {
+        if (data !== dataRef.current) {
+            sessionStorage.setItem('user', JSON.stringify(data.createUser))
+            console.log(JSON.parse(sessionStorage.getItem('user')))
+        }
+    }, [data, dataRef])
+    return(
+        <form onSubmit={e => {
+            e.preventDefault()
+            addUser()
+        }}>
+            <h3>Register</h3>
+            <div>
+                <div>
+                    <label>
+                        <p>Username:</p>
+                        <input placeholder='username' onChange={e => {
+                            setUsername(e.target.value)
+                        }}></input>
+                    </label>
+                    <label>
+                        <p>Password:</p>
+                        <input placeholder='password' onChange={e => {
+                            setPassword(e.target.value)
+                        }}></input>
+                    </label>
+                </div>
+                <button type='submit'>Create User</button>
+            </div>
+        </form>
+    )
+}
