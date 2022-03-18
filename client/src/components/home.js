fa0181de0839dcc6c4ad1203fdbb6fcd3be65dad
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-03-17 20:34:58 -0700

    login

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -3,14 +3,15 @@ import { useQuery } from '@apollo/client'
 import CreatePost from './createPost'
 import PostList from './postList'
 import findAll from '../queries/findAll'
-
+import Login from './login'
 export default function Home() {
+    const user = sessionStorage.getItem('user') ? JSON.parse(sessionStorage.getItem('user'))._id : false
     const { data } = useQuery(findAll, {
         pollInterval: 3000
     })
     return(
         <div>
-            <CreatePost/>
+            { user ? <CreatePost/> : <Login/> }
             { data ? <PostList data={data}/> : "Loading" }
         </div>
     )
