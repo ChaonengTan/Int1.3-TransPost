90134ce1981ab351511e7238f4ae4bff5c6894f1
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-03-17 20:34:44 -0700

    navbar username

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,13 +1,14 @@
 import logo from '../static/images/logo.png'
 import './styles/navbar.css'
 export default function Navbar() {
+    const username = sessionStorage.getItem('user') ? JSON.parse(sessionStorage.getItem('user')).username : false
     return(
         <div className='navbar'>
             <div className='navLeft'>
-                <a href="/"><img src={logo} alt='' />TransPost</a>
+                <a href="/"><img src={logo} alt=''/>TransPost</a>
             </div>
             <div className='navRight'>
-                login goes here
+                {username && username}
             </div>
         </div>
     )
