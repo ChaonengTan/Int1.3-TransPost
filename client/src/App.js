8831f717387035048484955fe4828401c873c821
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-03-17 21:05:15 -0700

    -footer

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,6 @@ function App() {
         <Navbar/>
         <Route exact path="/" component={Home}/>
         <Route path="/post/:id" component={Post}/>
-        <div>footer goes here</div>
       </div>
     </Router>
   );
