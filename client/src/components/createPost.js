cbe5ee2ff9035266293520b9fb4d068ba7e3a1b5
Chaoneng Tan <chaonengtan@Chaonengs-MacBook-Air.local>
2022-03-17 20:35:09 -0700

    createPost

--- client/src/components/createPost.js
+++ client/src/components/createPost.js
@@ -1,31 +1,35 @@
 import { useState } from 'react'
 import { useMutation } from '@apollo/client'
 import createPost from '../queries/createPost'
+import './styles/createPost.css'
 
 export default function CreatePost() {
+    const author = JSON.parse(sessionStorage.getItem('user')).username
     const [title, setTitle] = useState('')
     const [message, setMessage] = useState('')
     const [uploadPost] = useMutation(createPost, {
-        variables: { post: {title, message} }
+        variables: { post: {title, message, author} }
     })
     return(
-        <form onSubmit={e => {
+        <form className='createPost' onSubmit={e => {
             e.preventDefault()
             uploadPost()
         }}>
-            <label>
-                <h3>Title:</h3>
-                <input type='text' placeholder='Title' onChange={e => {
-                    setTitle(e.target.value)
-                }}></input>
-            </label>
-            <label>
-                <h3>Message:</h3>
-                <input type='text' placeholder='Message' onChange={e => {
-                    setMessage(e.target.value)
-                }}></input>
-            </label>
-            <button type='submit'>Submit</button>
+            <div>
+                <label>
+                    <h3>Title:</h3>
+                    <input type='text' placeholder='Title' onChange={e => {
+                        setTitle(e.target.value)
+                    }}></input>
+                </label>
+                <label>
+                    <h3>Message:</h3>
+                    <textarea placeholder='Message' onChange={e => {
+                        setMessage(e.target.value)
+                    }}></textarea>
+                </label>
+            </div>
+            <button type='submit'>Post!</button>
         </form>
     )
 }
